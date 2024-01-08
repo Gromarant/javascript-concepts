b73fa8da1fc52f941cb78b460c9fc2311ff6a7a3
Mariangelica Rodriguez <gromarant@gmail.com>
2024-01-08 20:25:02 +0100

    add/DOM practice

---
+++ DOM/main.js
@@ -0,0 +1,7 @@
+const link = document.querySelector("a");
+
+//change the link tag content
+link.textContent = "Mozilla Developer Network";
+
+//add url to the link
+link.href = "https://developer.mozilla.org";
